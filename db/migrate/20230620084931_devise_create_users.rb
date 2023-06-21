35deb92925690167a1c3d5b85490d04d09b83648
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-06-21 12:56:15 +0000

    [Start] kadai -3

--- db/migrate/20230620084931_devise_create_users.rb
+++ db/migrate/20230620084931_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -32,7 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name
+      t.text :introduction
       t.timestamps null: false
     end
 
