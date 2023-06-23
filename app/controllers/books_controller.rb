c825374ec5f20f9434e592453515bcecbceda600
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-06-23 08:51:35 +0000

    [Start] kadai -3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,35 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @user = current_user
+    @book = Book.new(book_params)
+    @book.user_id = (current_user.id)
+    @book.save
+    redirect_to books_path(@book.id)
   end
 
   def show
+    @user = current_user
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
   end
+
+  def index
+    @users =User.all
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
 end
